25536d67e7c854044df8a9fd5031503633b242af
Gobind Puniani <gobindpuniani@gmail.com>
2020-11-08 02:41:55 -0800

    Added deposit method

--- BankAccount.py
+++ BankAccount.py
@@ -5,7 +5,7 @@ class BankAccount:
     routing_number = 987654321
 
     def __init__(self, full_name):
-        # Pass in the name argument for the full name of the bank account number
+        # Pass in the function argument for the full name of the bank account number
         self.full_name = full_name
         # Each account has its own unique randomly generated 8-digit number
         # Line below inspired from this post: https://stackoverflow.com/questions/2673385/how-to-generate-random-number-with-the-specific-length-in-python
@@ -13,5 +13,12 @@ class BankAccount:
         # The account balance starts at $0
         self.balance = 0
 
+    # The deposit method adds the input `amount` to `balance` and prints out the amount deposited
+    def deposit(self, amount):
+        self.balance += amount
+        # Use .2f below to specify the float to have exactly 2 digits after the decimal point
+        print(f"Amount Deposited: ${self.balance:.2f}")
+
 joi = BankAccount("Joi Anderson")
 print(joi.account_number)
+joi.deposit(9.87)
