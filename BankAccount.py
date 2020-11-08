71d00cb31f70e149087a00eb1cb12dd23a0e32a0
Gobind Puniani <gobindpuniani@gmail.com>
2020-11-08 02:56:06 -0800

    Added withdraw method

--- BankAccount.py
+++ BankAccount.py
@@ -13,12 +13,25 @@ class BankAccount:
         # The account balance starts at $0
         self.balance = 0
 
-    # The deposit method adds the input `amount` to `balance` and prints out the amount deposited
+    # The `deposit` method adds the input `amount` to `balance` and prints out the amount deposited
     def deposit(self, amount):
         self.balance += amount
         # Use .2f below to specify the float to have exactly 2 digits after the decimal point
-        print(f"Amount Deposited: ${self.balance:.2f}")
+        print(f"Amount Deposited: ${amount:.2f}")
+
+    # The `withdraw` method subtracts the input `amount` from `balance` and prints out the amount withdrawn
+    # Attempted overdrafts will result in a printed message alerting the user and a charge of $10
+    def withdraw(self, amount):
+        if amount > self.balance:
+            self.balance -= 10
+            print("Insufficient funds. Your account has been charged an overdraft fee of $10.")
+        else:
+            self.balance -= amount
+            # Use .2f below to specify the float to have exactly 2 digits after the decimal point
+            print(f"Amount Withdrawn: ${amount:.2f}")
+
 
 joi = BankAccount("Joi Anderson")
 print(joi.account_number)
 joi.deposit(9.87)
+joi.withdraw(9)
